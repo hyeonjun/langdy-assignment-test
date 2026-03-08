141ae1a0f4823588886825ddbb514dc65b7c1829
hyeonjun <zlel175@naver.com>
2026-03-08 23:55:43 +0900

    feature: Add JpaAuditingConfig and refactor main application class
    - @EnableJpaAuditing 어노테이션을 LangdyApplication에서 분리
    - JpaAuditingConfig 클래스 추가로 JPA 감사 설정 전용 구성 파일 생성
    - 응집도 향상 및 설정 분리로 유지보수성 개선

--- src/main/kotlin/com/example/langdy/LangdyApplication.kt
+++ src/main/kotlin/com/example/langdy/LangdyApplication.kt
@@ -2,10 +2,8 @@ package com.example.langdy
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing
 
 @SpringBootApplication
-@EnableJpaAuditing
 class LangdyApplication
 
 fun main(args: Array<String>) {
