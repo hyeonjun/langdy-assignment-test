f5133077cd26035101930716b9e1084439f41fd9
hyeonjun <zlel175@naver.com>
2026-03-08 18:09:18 +0900

    init project

---
+++ src/main/kotlin/com/example/langdy/LangdyApplication.kt
@@ -0,0 +1,11 @@
+package com.example.langdy
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class LangdyApplication
+
+fun main(args: Array<String>) {
+    runApplication<LangdyApplication>(*args)
+}
---
+++ src/test/kotlin/com/example/langdy/LangdyApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.langdy
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class LangdyApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
