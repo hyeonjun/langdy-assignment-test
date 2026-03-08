1eb3dd6f6a79f59f4a81c43c58d85294a8825248
hyeonjun <zlel175@naver.com>
2026-03-08 21:38:07 +0900

    feature: Add BaseEntity and JPA auditing configuration
    - 공통 엔티티(BaseEntity) 클래스 추가
    - 생성일/수정일 자동 생성 및 업데이트 필드 포함
    - JPA Auditing 활성화를 위한 @EnableJpaAuditing 추가

---
+++ src/main/kotlin/com/example/langdy/domain/base/BaseEntity.kt
@@ -0,0 +1,32 @@
+package com.example.langdy.domain.base
+
+import com.fasterxml.jackson.annotation.JsonFormat
+import jakarta.persistence.Column
+import jakarta.persistence.EntityListeners
+import jakarta.persistence.GeneratedValue
+import jakarta.persistence.GenerationType
+import jakarta.persistence.Id
+import jakarta.persistence.MappedSuperclass
+import org.springframework.data.annotation.CreatedDate
+import org.springframework.data.annotation.LastModifiedDate
+import org.springframework.data.jpa.domain.support.AuditingEntityListener
+import java.time.LocalDateTime
+
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener::class)
+abstract class BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    val id: Long = 0
+
+    @Column(name = "`created_at`", nullable = false, updatable = false)
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+    @CreatedDate
+    var createdAt: LocalDateTime = LocalDateTime.now()
+
+    @Column(name = "`updated_at`", nullable = false)
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+    @LastModifiedDate
+    var updatedAt: LocalDateTime = LocalDateTime.now()
+}
